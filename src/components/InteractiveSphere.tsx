b5c362b5fcac926228e0f088be300c85f6db8e0c
JTStephens18 <jtstephens18@gmail.com>
2026-01-21 15:37:03 -0600

    shader color changes over time

--- src/components/InteractiveSphere.tsx
+++ src/components/InteractiveSphere.tsx
@@ -2,25 +2,38 @@ import { Sphere } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
 import React, { type FC, useMemo, useState } from "react";
 import { MathUtils } from "three";
-import { color, mix, positionWorld, uniform, uv, vec3 } from "three/tsl"
+import { color, mix, positionWorld, positionLocal, uniform, uv, vec3, sin, greaterThan, select, equal } from "three/tsl"
 
 const InteractiveSphere: FC = () => {
     const [isPointerOver, setIsPointerOver] = useState(false);
 
-    const { key, colorNode, positionNode, scaleNode, uHovered } = useMemo(() => {
+    const { key, colorNode, positionNode, uHovered, uTime } = useMemo(() => {
         const uHovered = uniform(0.0);
 
-        const defaultColor = mix(color("#3F4A4B"), color("#7A8BBC"), uv().y);
-        const hoverColor = mix(color("#14DCE9"), color("#B462D1"), uv().y);
+        const uTime = uniform(0.0);
 
-        const colorNode = mix(defaultColor, hoverColor, uHovered);
+        // New: range is [0 to 1]
+        // We multiply by 0.5 (to get -0.5 to 0.5) and add 0.5 (to get 0.0 to 1.0)
+        // const gradientFactor = sin(positionLocal.length().mul(20.0).add(uTime)).mul(0.5).add(0.5);
+        const gradientFactor = sin(positionLocal.length().mul(20.0).add(uTime.mul(4.0))).mul(0.5).add(0.5).round();
 
-        const positionNode = positionWorld.sub(vec3(0, 0, uHovered.oneMinus()));
+        // const gradientFactor = uv().y.add(uTime).mul(0.5).add(0.5);
+
+        const defaultColor = mix(color("rgba(56, 179, 56, 1)"), color("rgba(4, 18, 211, 1)"), gradientFactor);
+        // const defaultColor = color("rgba(56, 179, 56, 1)");
+        const hoverColor = mix(color("rgba(255, 100, 0, 1)"), color("rgba(75, 0, 130, 1)"), gradientFactor);
+        // const hoverColor = color("rgba(255, 0, 0, 1)");
+
+        // const colorNode = defaultColor;
+        // const colorNode = mix(defaultColor, hoverColor, uHovered);
+        const colorNode = mix(hoverColor, defaultColor, uHovered);
+
+        const positionNode = positionWorld.sub(vec3(0, 0, 0.7));
 
         const scaleNode = vec3(1.0, 1.0, 1.0).add(vec3(0.0, 0.0, uHovered));
 
         const key = colorNode.uuid;
-        return { key, colorNode, positionNode, scaleNode, uHovered };
+        return { key, colorNode, positionNode, scaleNode, uHovered, uTime };
     }, []);
 
     useFrame((_, delta) => {
@@ -30,6 +43,7 @@ const InteractiveSphere: FC = () => {
             5,
             delta
         );
+        uTime.value += delta;
     });
 
     return (
@@ -50,7 +64,6 @@ const InteractiveSphere: FC = () => {
                 colorNode={colorNode}
                 positionNode={positionNode}
                 shininess={100}
-            // scaleNode={scaleNode}
             />
 
         </Sphere>
